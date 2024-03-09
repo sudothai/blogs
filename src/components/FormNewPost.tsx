551287fa8195e2d257ed41db1aaa6a225639ebc9
Thai Radin <thai.radin@outlook.com>
2024-03-08 23:15:45 -0500

    finally works

--- src/components/FormNewPost.tsx
+++ src/components/FormNewPost.tsx
@@ -1,16 +1,12 @@
 "use client"
 
 import { FormData } from "@/types/blogs"
-import { useState } from "react"
+import { ChangeEvent, FormEvent, useState } from "react"
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 const inputClass = "w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-const handleChange = (e: ChangeEvent<HTMLInputElement) => {
-}
- 
-export default ;
 
 const FormNewPost = () => {
     const [formData, setFormData] = useState<FormData>({
@@ -18,8 +14,24 @@ const FormNewPost = () => {
         content: "",
     })
 
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        e.preventDefault();
+        const { name, value } = e.target
+        setFormData({
+            ...formData,
+            [name]: value,
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log("Form Submitted")
+        console.log(formData)
+    }
+
+
     return (
-       <form action="" className="max-w-md mx-auto p-4">
+       <form action="" className="max-w-md mx-auto p-4" onSubmit={handleSubmit}>
         <div className="mb-4">
             <Input type="text" className={inputClass} placeholder="Enter the title" name="title" value={formData.title} onChange={handleChange} />
         </div>
